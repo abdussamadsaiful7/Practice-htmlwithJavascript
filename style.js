47b108d83e2e3f1e629fe202aa303168a78c9b91
abdussamadsaiful7 <samadsaifullah11@gmail.com>
2023-02-11 13:34:38 +0600

    added orange button

--- style.js
+++ style.js
@@ -10,4 +10,9 @@ const makePinkButton = document.getElementById('make-pink');
         makePinkButton.onclick = makePink;
         function makePink (){
             document.body.style.backgroundColor = 'pink';
-        }
+        };
+
+    const makeGrayButton = document.getElementById('make-gray');
+        makeGrayButton.onclick = function(){
+            document.body.style.backgroundColor = 'gray';
+        };
