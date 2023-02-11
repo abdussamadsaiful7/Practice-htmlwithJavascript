72da47bde0071d3ef1a41c5d4012dda6be0c2185
abdussamadsaiful7 <samadsaifullah11@gmail.com>
2023-02-11 12:56:40 +0600

    added admit button

---
+++ style.js
@@ -0,0 +1,13 @@
+
+const makeBlueButton = document.getElementById('make-blue');
+        makeBlueButton.onclick = makeBlue;
+        function makeBlue (){
+            document.body.style.backgroundColor = 'blue';
+
+        };
+
+const makePinkButton = document.getElementById('make-pink');
+        makePinkButton.onclick = makePink;
+        function makePink (){
+            document.body.style.backgroundColor = 'pink';
+        }
